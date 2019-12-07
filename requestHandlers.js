cbc1cf4148c8b53aaa9afad1e2672808fb0a17d1
drmartell <drmartell@gmail.com>
2019-12-07 12:09:25 -0800

    res undefined p.35

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,29 @@
-const exec = require('child_process').exec;
-
 const start = res => {
   console.log('Request handler "start" was called.');
 
-  exec('ls -lah', (error, stdout) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(stdout);
-    res.end();
-  });
+  const body = '<html>' +
+  '<head>' +
+  '<meta http-equiv="Content-Type" content="text/html; ' +
+  'charset=UTF-8" />' +
+  '</head>' +
+  '<body>' +
+  '<form action="/upload" method="post">' +
+  '<textarea name="text" rows="20" cols="60"></textarea>' +
+  '<input type="submit" value="Submit text" />' +
+  '</form>' +
+  '</body>' +
+  '</html>';
+  
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write(body);
+  res.end();
 };
 
-const upload = () => {
+const upload = res => {
   console.log('Request handler "upload" was called.');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Upload');
+  res.end();
 };
 
 exports.start = start;
--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const url = require('url');
 
 exports.start = (route, handle) => {
   http.createServer((req, res) => {
+    let postData = '';
     const pathName = url.parse(req.url).pathname;
     console.log(`Request for ${pathName} received.`);
 
-    route(handle, pathName, res);
+    req.setEncoding('utf8');
+
+    req.addListener('data', postDataChunk => {
+      postData += postDataChunk;
+      console.log('Received POST data chunk "' + postDataChunk + '".');
+    });
+
+    req.addListener('end', () => route(handle, pathName, res, postData));
+
   }).listen(8888);
   
   console.log('Server has started.');
